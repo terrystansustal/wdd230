e1aa302482236711f27fa6be8a6b29204ff18b3d
terrystansustal <terrysustal@gmail.com>
2023-02-02 13:43:06 -0800

    Fixed the hamburger nav menu

--- course-homepage/chamber/scripts/hamburger.js
+++ course-homepage/chamber/scripts/hamburger.js
@@ -1,6 +1,10 @@
 const toggleMenu = function() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.getElementById("logo").classList.toggle("close");
+    document.getElementById("title").classList.toggle("close");
+    document.getElementById("date").classList.toggle("close");
+    document.getElementById("social").classList.toggle("close");
 }
 
 const x = document.getElementById("hamburgerBtn");
