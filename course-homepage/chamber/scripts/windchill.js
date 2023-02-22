8ce13de557368632fe9c2373400d16b5a38bc7fc
terrystansustal <terrysustal@gmail.com>
2023-02-22 09:13:31 -0800

    Fixed w06 mistakes

--- course-homepage/chamber/scripts/windchill.js
+++ course-homepage/chamber/scripts/windchill.js
@@ -6,19 +6,22 @@
 
 const temp = document.getElementById("temperature");
 const windSpeed = document.getElementById("wind-speed");
+const tempValue = parseFloat(temp.innerHTML);
+const windSpeedValue = parseFloat(windSpeed.innerHTML);
 
 // check to make sure they meet the specification limits  (<=50°F and >3.0mph)
-if (temp <=50 && windSpeed > 3)
+if (tempValue <=50 && windSpeedValue > 3)
 {
-    const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+    const windChill = 35.74 + 0.6215 * tempValue - 35.75 * Math.pow(windSpeedValue, 0.16) + 0.4275 * tempValue * Math.pow(windSpeedValue, 0.16);
 
     // Display the wind chill
-    document.getElementById("wind-chill").textContent = windChill;
+    document.getElementById("wind-chill").innerHTML = Math.round(windChill * 10) / 10;
+    console.log(windChill);
 }
 
 else
 {
-    // document.getElementById("wind-chill)").textContent = "N/A";
-    document.getElementById("wind-chill)");
+    const otherNumber = "N/A";
+    document.getElementById("wind-chill)").innerHTML = otherNumber;
 }
 
